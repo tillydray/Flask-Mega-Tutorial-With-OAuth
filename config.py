04a63faa7200a4dcc2f5ad157141cc2dbcd74b11
Jason Fry <jason@feathr.co>
2017-03-02 17:00:50 -0500

    Add OAuth credentials for Facebook and Twitter

--- config.py
+++ config.py
@@ -4,6 +4,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+app.config['OAUTH_CREDENTIALS'] = {
+    'facebook': {
+        'id': '1837196166539923',
+        'secret': 'b686b5a304e204587536042acc1fc566'
+     },
+    'twitter': {
+        'id': '5AX8vQD41aF7btSPLJkIiuJKd',
+        'secret': 'aQNQRQsPfRD3Fpz0pSgwiwyftlOZWp22b5alipQzeCakxXLM3t'
+    }
+}
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
