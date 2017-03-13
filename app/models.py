110898568254e916a8ad77264c1f1ed9683e93dc
Jason Fry <jason@feathr.co>
2017-03-13 08:54:52 -0400

    Add OAuth login capability

--- app/models.py
+++ app/models.py
@@ -1,11 +1,17 @@
-from app import db
+from app import db, lm
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # posts = db.relationship('Post', backref='author', lazy='dynamic')
+    social_id = db.Column(db.String(64), nullable=Flase, unique=True)
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
     @property
     def is_authenticated(self):
