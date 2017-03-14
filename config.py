5fa3b20e8d74bb6285f91075b822465df7eeaa00
Jason Fry <jason@feathr.co>
2017-03-14 13:06:32 -0400

    Add comment explaining code change from tutorial

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-OAUTH_CREDENTIALS = {
+OAUTH_CREDENTIALS = { #tutorial has app.config['OAUTH_CRED...']' but that's unnecessary
     'facebook': {
         'id': '1837196166539923',
         'secret': 'b686b5a304e204587536042acc1fc566'
