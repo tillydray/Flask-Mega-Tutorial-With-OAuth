4f1b454d3fc7a3279b1eb642a9a6bec3bebfed25
Jason Fry <jason@feathr.co>
2017-03-14 11:42:39 -0400

    Comment extraneous code from Flask Mega Tutorial

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_openid import OpenID
+# from flask_openid import OpenID
 from config import basedir
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
--- app/views.py
+++ app/views.py
@@ -1,14 +1,14 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
-from .forms import LoginForm
+from app import app#, db, lm, oid
+# from .forms import LoginForm
 from .models import User
 from oauth import OAuthSignIn
 
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+# @lm.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
 
 
 @app.before_request
@@ -37,30 +37,30 @@ def index():
                            posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
+@app.route('/login', methods=['GET'])#, 'POST'])
+# @oid.loginhandler
 def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    # if g.user is not None and g.user.is_authenticated:
+    #     return redirect(url_for('index'))
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     session['remember_me'] = form.remember_me.data
+    #     return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+                           title='Sign In')#,
+                           # form=form,
+                           # providers=app.config['OPENID_PROVIDERS'])
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
-    if not current_user.is_anonymous:
+    if not current_user.is_anonymous: #tutorial has is_anonymous(), but throws bool error.
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
     return oauth.authorize()
 
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
-    if not current_user.is_anonymous:
+    if not current_user.is_anonymous: #tutorial has is_anonymous(), but throws bool error.
         return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
     social_id, username, email = oauth.callback()
@@ -75,25 +75,25 @@ def oauth_callback(provider):
     login_user(user, True)
     return redirect(url_for('index'))
 
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
+# @oid.after_login
+# def after_login(resp):
+#     if resp.email is None or resp.email == "":
+#         flash('Invalid login. Please try again.')
+#         return redirect(url_for('login'))
+#     user = User.query.filter_by(email=resp.email).first()
+#     if user is None:
+#         nickname = resp.nickname
+#         if nickname is None or nickname == "":
+#             nickname = resp.email.split('@')[0]
+#         user = User(nickname=nickname, email=resp.email)
+#         db.session.add(user)
+#         db.session.commit()
+#     remember_me = False
+#     if 'remember_me' in session:
+#         remember_me = session['remember_me']
+#         session.pop('remember_me', None)
+#     login_user(user, remember=remember_me)
+#     return redirect(request.args.get('next') or url_for('index'))
 
 
 @app.route('/logout')
