0ede89f771cd6c592549df6bc7ae8f57ed0d8b7d
Jason Fry <jason@feathr.co>
2017-03-14 13:35:07 -0400

    Fix a name error that occurred when a user first used the web app

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from app import app#, db, lm, oid
+from app import app, db#, lm, oid
 # from .forms import LoginForm
 from .models import User
 from oauth import OAuthSignIn
